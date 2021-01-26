0d846c3eb0399164a8619dfa2bb732a306b2ad7d
Luis <luisale9826@gmail.com>
2021-01-26 16:00:58 -0600

    Se termina el index

--- index.php
+++ index.php
@@ -8,30 +8,15 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Rancho&display=swap" rel="stylesheet">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <title>Document</title>
 </head>
 
 <body>
+    <?php
+    include 'header.php';
+    ?>
     <div class="container-fluid">
-        <header>
-            <div class="row">
-                <div class="col-6">
-                    <img class="logo" src="./assets/DestinosNomada.png" alt="">
-                </div>
-                <div class="col-6 align-self-center">
-                    <div class="row justify-content-end">
-                        <div class="col-2">
-                            <a>Destinos</a>
-                        </div>
-                        <div class="col-3">
-                            <a>Iniciar Sesión</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </header>
         <div>
             <div>
                 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
@@ -85,24 +70,11 @@
                 </div>
             </div>
         </div>
-        <footer id="footer">
-            <div id="texto" class="row">
-                <div class="col-2">
-                    <p>Contactenos</p>
-                    <p>9050-1050</p>
-                </div>
-                <div class="col-2">
-                    <p>Correo</p>
-                    <p>destinosNom@gmail.com</p>
-                </div>
-            </div>
-        </footer>
     </div>
+    <?php include 'footer.php'; ?>
 </body>
 <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
 
 </html>
